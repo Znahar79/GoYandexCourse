d693e36eefca5ebc9bdb8a1499c612576594445e
Artem Smirnov <asmirnov@pos-soft.ru>
2025-02-15 14:01:34 +0300

    Push estimate value testing

--- MathTesting/math_test.go
+++ MathTesting/math_test.go
@@ -1,6 +1,10 @@
 package math
 
-import "testing"
+import (
+	"testing"
+
+	"github.com/stretchr/testify/assert"
+)
 
 func TestAddPositive(t *testing.T) {
 	sum, err := Add(1, 2)
@@ -39,5 +43,50 @@ func TestAddZero(t *testing.T) {
 	_, err = Add(0, 0)
 	if err == nil {
 		t.Error("all arg negative - expected error not be nil")
+	}
+}
+
+func TestEstimateValueSmall(t *testing.T) {
+	result := EstimateValue(9)
+	assert.Equal(t, "small", result)
+}
+
+func TestEstimateValueMedium(t *testing.T) {
+	result := EstimateValue(99)
+	assert.Equal(t, "medium", result)
+}
+
+func TestEstimateValueBig(t *testing.T) {
+	result := EstimateValue(100)
+	assert.Equal(t, "big", result)
+}
+
+func TestEstimateValueTableDriven(t *testing.T) {
+	testCases := []struct {
+		Name          string
+		InputValue    int
+		ExpectedValue string
+	}{
+		{
+			Name:          "Small",
+			InputValue:    9,
+			ExpectedValue: "small",
+		},
+		{
+			Name:          "Medium",
+			InputValue:    99,
+			ExpectedValue: "medium",
+		},
+		{
+			Name:          "Big",
+			InputValue:    100,
+			ExpectedValue: "big",
+		},
+	}
+
+	for _, tc := range testCases {
+		t.Run(tc.Name, func(t *testing.T) {
+			assert.EqualValues(t, tc.ExpectedValue, EstimateValue(tc.InputValue))
+		})
 	}
 }
